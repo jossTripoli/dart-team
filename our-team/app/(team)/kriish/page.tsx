055f9d715d2b1392447411cd4a78b5e0b2a33273
Joss <78896064+jossTripoli@users.noreply.github.com>
2025-02-04 11:19:52 -0500

    Delete our-team directory

--- our-team/app/(team)/kriish/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function KriishPage() {
-  return (
-    <div className="min-h-screen p-8 bg-gray-50 flex flex-col items-center">
-      <h1 className="text-4xl font-bold text-indigo-600 mb-4">Kriish</h1>
-    </div>
-  );
-}
--- our-team/components/header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <header className="h-20 w-full border-b-2 border-neutral-200 px-4">
-      Our Team
-    </header>
-  );
-}
